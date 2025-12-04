7ab8651a410efd453c4c405525826c8e3e9b68ab
samiksha-1203 <samikshaa1902@gmail.com>
2025-12-04 22:26:18 +0530

    Auto-create superuser from env in app ready()

--- helpdesk/ticketsapp/apps.py
+++ helpdesk/ticketsapp/apps.py
@@ -6,5 +6,16 @@ class TicketsappConfig(AppConfig):
     name = 'ticketsapp'
 
     def ready(self):
-        # Import signal handlers
         from . import signals  # noqa: F401
+        import os
+        from django.contrib.auth import get_user_model
+        u = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+        p = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+        e = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+        if u and p and e:
+            User = get_user_model()
+            if not User.objects.filter(username=u).exists():
+                try:
+                    User.objects.create_superuser(username=u, email=e, password=p)
+                except Exception:
+                    pass
