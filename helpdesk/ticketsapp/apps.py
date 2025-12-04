970755ce9fce5d25bd2eddbaa0a0b2a4537c8e29
samiksha-1203 <samikshaa1902@gmail.com>
2025-12-04 22:36:40 +0530

    Create admin on post_migrate using env vars

--- helpdesk/ticketsapp/apps.py
+++ helpdesk/ticketsapp/apps.py
@@ -7,15 +7,21 @@ class TicketsappConfig(AppConfig):
 
     def ready(self):
         from . import signals  # noqa: F401
-        import os
-        from django.contrib.auth import get_user_model
-        u = os.environ.get('DJANGO_SUPERUSER_USERNAME')
-        p = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
-        e = os.environ.get('DJANGO_SUPERUSER_EMAIL')
-        if u and p and e:
-            User = get_user_model()
-            if not User.objects.filter(username=u).exists():
-                try:
+        from django.db.models.signals import post_migrate
+        from django.apps import apps
+
+        def _bootstrap_admin(**kwargs):
+            import os
+            u = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+            p = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+            e = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+            if not (u and p and e):
+                return
+            User = apps.get_model('auth', 'User')
+            try:
+                if not User.objects.filter(username=u).exists():
                     User.objects.create_superuser(username=u, email=e, password=p)
-                except Exception:
-                    pass
+            except Exception:
+                pass
+
+        post_migrate.connect(_bootstrap_admin, dispatch_uid='ticketsapp_bootstrap_admin')
